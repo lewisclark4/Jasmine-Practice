1ba006acbd5fc8b10a152510d093e7d2942fd5be
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-02-06 14:09:51 +0000

    added calc addition funtion and Jasmine tests

---
+++ spec/calcSpec.js
@@ -0,0 +1,30 @@
+// calc test
+
+describe("Calculator", function() {
+    beforeEach(function() {
+        calc = new Calculator;
+    });
+    
+    describe("Addition Test", function() {
+        it ("should return 42", function() {
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42)
+        });
+        it ("should return 26", function() {
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26)
+        });
+        it ("should return 15", function() {
+            calc.add(5);
+            calc.add(10);
+            expect(calc.value).toBe(15)
+        });
+        it ("should return an error if we don't supply two numbers", function() {
+            spyOn(window, "alert");
+            calc.add("Hitchhikers")
+            expect(window.alert).toHaveBeenCalledWith("Error")
+        });
+    }); 
+});
